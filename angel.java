0029a8c01e3803de844ed4a8127f50212d79533b
jairi198902 <39605625+jairi198902@users.noreply.github.com>
2018-05-31 20:06:10 -0500

    Add files via upload

---
+++ angel.java
@@ -0,0 +1,30 @@
+package angel;
+
+import javax.swing.JOptionPane;
+
+public class angel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+public class principal {
+	public static void main (String[] args){
+		int op=0, n1=0, n2=0;
+		String ax;
+		operaciones trin= new operaciones();
+		do{
+			op=JOptionPane.showConfirmDialog(null,"Desea ingresar dos numeros y mostrar el resultado"
+					+"nde de las 4operaciones basicas?"
+					if(op == JOptionPane.YES_OPTION){
+						ax = JOptionPane.showInputDialog(null,"Digite el primer numero:  ")
+								if(trin.isNum(ax)){
+									n1= Integer .parseInt(ax):
+										ax= JOptionPane.ShowInputDialog(null,"Digite el segundo numero:    "
+												if(trin
+								}
+					}
+		}
+	}
+}
+	}
+
+}
